0e3d09b4ad13b4f04a996f93b0240d093001d0e8
insommmnia <82572103+insommmnia@users.noreply.github.com>
2022-04-07 13:02:57 +0900

    Add files via upload

---
+++ login/addevent.php
@@ -0,0 +1,116 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Предложить мероприятие</title>
+	<meta charset="UTF-8">
+	<link rel="icon" type="image/x-icon" href="/img/fav_ico.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+				<form method="POST" enctype="multipart/form-data" action="../php/addevent.php" class="login100-form validate-form flex-sb flex-w">
+					<span class="login100-form-title p-b-32">
+						Предложить дело
+					</span>
+
+					<span class="txt1 p-b-11">
+						Название
+					</span>
+					<div class="wrap-input100 validate-input m-b-36" data-validate = "Username is required">
+						<input class="input100" type="text" name="name" >
+						<span class="focus-input100"></span>
+					</div>
+					
+					<span class="txt1 p-b-11">
+						Описание
+					</span>
+					<div class="wrap-input100 validate-input m-b-12" data-validate = "Password is required">
+						
+						<input class="input100" type="" name="descr" >
+						<span class="focus-input100"></span>
+					</div>
+
+                    <span class="txt1 p-b-11">
+						Место
+					</span>
+					<div class="wrap-input100 validate-input m-b-12" data-validate = "Password is required">
+						
+						<input class="input100" type="" name="place" >
+						<span class="focus-input100"></span>
+					</div>
+
+					<span class="txt1 p-b-11">
+						Фото
+					</span>
+					<div class="wrap-input100 validate-input m-b-12" data-validate = "Password is required">
+						
+						<div class="form-group mt-3">
+           									<label class="mr-2">Загрузить картинку</label>
+            								<input type="file" name="img">
+          					</div>
+						<span class="focus-input100"></span>
+					</div>
+					
+                    
+
+					<div class="container-login100-form-btn">
+						<button type="submit" class="login100-form-btn">
+							Предложить
+						</button>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
